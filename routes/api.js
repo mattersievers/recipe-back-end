64bc64d1f2f7b79c65dd7a816978fa5524746d86
Matthew Sievers <MatterSievers@gmail.com>
2022-04-23 16:44:00 -0500

    completed step 2

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,18 @@ router.get("/recipes", (req,res) => {
     let data = {recipeNames: []};
     database.recipes.forEach(recipe => data.recipeNames.push(recipe.name));
     res.json(data);
-})
+});
+
+router.get("/recipes/details/:name", (req, res) => {
+    let data = {};
+    let details = {};
+    database.recipes.forEach(recipe => {
+        if(recipe.name === req.params.name){
+            details = {ingredients: recipe.ingredients, numSteps: recipe.instructions.length}
+            data = {details};
+        };
+    })    
+    res.json(data);
+});
 
 module.exports = router;
